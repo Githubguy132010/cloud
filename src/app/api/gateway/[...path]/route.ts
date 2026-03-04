ac8c837da744607e698ec538c9284a5423710cdc
Christiaan Arnoldus <christiaan.arnoldus@outlook.com>
2026-03-04 15:44:32 +0100

    Unclear whether modifying next headers is allowed, use simpler approach

--- src/app/api/gateway/[...path]/route.ts
+++ src/app/api/gateway/[...path]/route.ts
@@ -1,10 +1,1 @@
-import { POST as openrouterPOST } from '@/app/api/openrouter/[...path]/route';
-import type { NextRequest } from 'next/server';
-import { FEATURE_HEADER } from '@/lib/feature-detection';
-
-export function POST(request: NextRequest) {
-  if (!request.headers.has(FEATURE_HEADER)) {
-    request.headers.set(FEATURE_HEADER, 'direct-gateway');
-  }
-  return openrouterPOST(request);
-}
+export { POST } from '@/app/api/openrouter/[...path]/route';
