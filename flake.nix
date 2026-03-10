83809033ad1796a6857953d819b6a176fd1a4986
Catriel Müller <catrielmuller@gmail.com>
2026-03-10 10:17:45 -0300

    feat: add vercel to nix dev env

--- flake.nix
+++ flake.nix
@@ -5,33 +5,43 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
   };
 
-  outputs = { self, nixpkgs, ... }: let
-    systems = [ "aarch64-darwin" "x86_64-linux" ];
-
-    forAllSystems = nixpkgs.lib.genAttrs systems;
-
-    mkDevShell = system: let
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      };
-    in pkgs.mkShell {
-      name = "kilo-code-backend";
-
-      packages = with pkgs; [
-        git
-        git-lfs
-        nodejs_22
-        corepack_22
-        dotenvx
-        _1password-cli
-        postgresql_18
-        wrangler
+  outputs =
+    { self, nixpkgs, ... }:
+    let
+      systems = [
+        "aarch64-darwin"
+        "x86_64-linux"
       ];
+
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+
+      mkDevShell =
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+          };
+        in
+        pkgs.mkShell {
+          name = "kilo-code-backend";
+
+          packages = with pkgs; [
+            git
+            git-lfs
+            nodejs_22
+            corepack_22
+            dotenvx
+            _1password-cli
+            postgresql_18
+            wrangler
+            nodePackages.vercel
+          ];
+        };
+    in
+    {
+      devShells = forAllSystems (system: {
+        default = mkDevShell system;
+      });
     };
-  in {
-    devShells = forAllSystems (system: {
-      default = mkDevShell system;
-    });
-  };
 }
